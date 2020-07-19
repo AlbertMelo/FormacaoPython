7b9f2a7867d442736d0ef569baa5e4f232f951b6
AlbertMelo <menos.albert1981@gmail.com>
2020-07-19 18:04:48 -0400

    Python 3: Introdução a Orientação a objetos Aula 1 - Atividade

---
+++ OO/Teste.py
@@ -0,0 +1,18 @@
+
+'''
+Python 3: Introdução a Orientação a objetos
+Atividade - Aula 1
+'''
+def cria_conta(numero, titular, saldo, limite):
+    conta = { "numero": numero, "titular": titular, "saldo":saldo, "limite": limite }
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo {}".format(conta["saldo"]))
+
