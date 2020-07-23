dfb3938c7d7b21d3c09fb24bf3e813c8bd8c588f
AlbertMelo <menos.albert1981@gmail.com>
2020-07-22 21:07:05 -0400

    Formacao Python - Aula 1

--- OO/Aula1/teste.py
+++ OO/Aula1/teste.py
@@ -3,10 +3,10 @@
 Python 3: Introdução a Orientação a objetos
 Atividade - Aula 1 - 19-07-2020
 '''
+
 def cria_conta(numero, titular, saldo, limite):
     conta = { "numero": numero, "titular": titular, "saldo":saldo, "limite": limite }
     return conta
-
 def deposita(conta, valor):
     conta["saldo"] += valor
 
