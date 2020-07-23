6b9c7385ec26b6fc736dbda2d707c1799ed67111
AlbertMelo <menos.albert1981@gmail.com>
2020-07-22 20:55:18 -0400

    Aula 4 - Atributos privados e transferência de valores entre contas

---
+++ OO/Aula4/conta.py
@@ -0,0 +1,25 @@
+'''
+Mãos na massa: Atributos privados e transferência de valores entre contas
+'''
+
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__numero, self.__saldo))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
